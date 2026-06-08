2e8026ba85c256a267bf5e8035b34c1c2d5c91f5
wangmuxi-luchi <18593825310@163.com>
2026-06-08 16:43:54 +0800

    Update version to 0.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.wy.simple_timer"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.0.5"
+        versionCode = 2
+        versionName = "0.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
